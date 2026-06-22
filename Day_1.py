0faf89050e4c88eb135b4aa3046d2053513f184e
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-06-22 12:16:34 +0530

    Update Day_1.py

--- Day_1.py
+++ Day_1.py
@@ -5,3 +5,23 @@ for i in x: # for each
 
 for i in range (len(x)): #fpr loop
   print(i)
+
+x = [10, 20, 30, 40, 50]
+for i in x: # for each loop
+    print(i)
+
+for i in range (len(x)):  #for loop
+    print(x[i])
+
+
+# now you have to print their index at the same line using for loop.
+
+for i in range(len(x)):
+    # index, value
+    print(f"Index: {i}, Value: {x[i]}")
+
+
+#enumerate problem
+
+for index,elem in enumerate(x):
+    print(f"Index: {index}, Value: {elem}")
